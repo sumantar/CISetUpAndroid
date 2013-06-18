9df627ce54ea64dbbb150566f5a5383824f95ad8
sumanta <rout.sumanta@gmail.com>
2013-06-18 13:01:09 +0530

    First Commit

---
+++ SampleTestUnitTest/src/com/jenkinsCI/sampletest/test/Testing.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.jenkinsCI.sampletest.test;
+
+
+import org.junit.After;
+import org.junit.Before;
+
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+/**
+ * @author sumanta
+ *
+ */
+public class Testing extends AndroidTestCase {
+
+	/**
+	 * @param name
+	 */
+	public Testing() {
+		super();
+	}
+
+	/* (non-Javadoc)
+	 * @see android.test.AndroidTestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see android.test.AndroidTestCase#tearDown()
+	 */
+	@After
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@SmallTest
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
